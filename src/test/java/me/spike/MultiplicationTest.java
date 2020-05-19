87970c267d4957b283e2010a6f2549a201b18445
Ajay <ajaydi@thoughtworks.com>
2020-05-19 10:38:06 +0100

    Added JUnit5 and JUnit4 tests

---
+++ src/test/java/me/spike/MultiplicationTest.java
@@ -0,0 +1,14 @@
+package me.spike;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplicationTest {
+
+    @Test
+    public void subtractTwoNumbers() {
+        assertEquals(8, 4 * 2);
+    }
+}
--- src/test/java/me/spike/SubtractionTest.java
+++ src/test/java/me/spike/SubtractionTest.java
@@ -1,8 +1,10 @@
 package me.spike;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class SubtractionTest {
 
